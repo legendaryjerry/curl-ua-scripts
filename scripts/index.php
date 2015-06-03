78c2d073260f014342ae058b4d14a2df0885a627
Jerry <legendaryjerry@users.noreply.github.com>
2015-06-02 22:01:54 -0500

    Create index.php

---
+++ scripts/index.php
@@ -0,0 +1,12 @@
+Files
+-----
+* aa           Memory Usage Details
+* ab           Counts unique visitors from Apache access_log
+* dk           Shows large files
+* sysinfo      Link to bashfu.html, shows basic system information
+* tree         List directory tree with nice formatting
+* verify       Lists CPUs, Memory
+* rkhunt       Installs and runs rootkit hunter
+* ckhttp       Basic HTTP Maths
+* ckapache2    
+* ip.php       Shows your public IP address
